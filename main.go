8b27c12fc7b02639d06ad312d7cb81bd175bdba4
sing3demons <sing3demons@live.com>
2022-10-05 00:39:00 +0700

    fix: check dotenv

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/sing3demons/pos-app/db"
 )
 
 func init() {
-	db.ConnectDB()
+	if os.Getenv("GIN_MODE") != gin.ReleaseMode {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
 }
 
 func main() {
-
+	db.ConnectDB()
 	os.MkdirAll("uploads/products", 0755)
-	gin.SetMode(gin.DebugMode)
+	
 	r := gin.Default()
 	r.Static("/uploads", "./uploads")
 	serveRoutes(r)

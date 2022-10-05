0c2a5bafd179343e75140db6639490c5706faf7d
sing3demons <sing3demons@live.com>
2022-10-05 09:02:55 +0700

    fix: graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -15,16 +21,45 @@ func init() {
 			log.Fatal("Error loading .env file")
 		}
 	}
-
 }
 
 func main() {
 	db.ConnectDB()
 	os.MkdirAll("uploads/products", 0755)
-	
+
 	r := gin.Default()
 	r.Static("/uploads", "./uploads")
 	serveRoutes(r)
 
-	r.Run(":8080")
+	listenAndServe(r, "8080")
+}
+
+func listenAndServe(r *gin.Engine, port string) {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	s := &http.Server{
+		Addr:           ":" + port,
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
 }

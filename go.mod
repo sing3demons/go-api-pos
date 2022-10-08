d891d82fcc2ab5e0d0f80aacdcfb1f6c02684ba2
sing3demons <sing3demons@live.com>
2022-10-08 09:33:35 +0700

    add env example

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sing3demons/pos-app
 go 1.19
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sing3demons/pos-app/db"
@@ -25,11 +26,19 @@ func init() {
 
 func main() {
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	db.ConnectDB()
 	db.Migration()
 	os.MkdirAll("uploads/products", 0755)
 
 	r := gin.Default()
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+
+	r.Use(cors.New(corsConfig))
 	r.Static("/uploads", "./uploads")
 	serveRoutes(r)
 
